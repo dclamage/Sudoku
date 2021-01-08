76eb7e3348fcac1f222f1494128c0d6a63863b83
David Clamage <rangsk@gmail.com>
2021-01-08 04:41:55 -0800

    Added spinner instead of "Loading..." text
    Moved javascript interop functions to a new interop.js file.

---
+++ wwwroot/scripts/interop.js
@@ -0,0 +1,13 @@
+﻿window.getSVG_XY = (svg, x, y) => {
+    var pt = svg.createSVGPoint();
+    pt.x = x;
+    pt.y = y;
+    pt = pt.matrixTransform(svg.getScreenCTM().inverse());
+    return pt.x + " " + pt.y;
+};
+window.getBoundingClientRect = (element) => {
+    return element.getBoundingClientRect();
+};
+window.setFocusToElement = (element) => {
+    element.focus();
+};
