e2bed818755cad6a421c7cdacbef228567e431e4
dclamage <dclamage@users.noreply.github.com>
2021-01-18 03:35:09 +0000

    Deploying to gh-pages from  @ ddd11711b4f804a7d8af8bdd1056eedbf7893cfb 🚀

--- scripts/interop.js
+++ scripts/interop.js
@@ -11,12 +11,39 @@ window.getBoundingClientRect = (element) => {
 window.setFocusToElement = (element) => {
     element.focus();
 };
-window.setSidebarSize = (element) => {
-    document.documentElement.style.setProperty(`--sidebar-width`, $(element).outerWidth(true));
-    document.documentElement.style.setProperty(`--sidebar-height`, $(element).outerHeight(true));
+window.keypadWidth = 0;
+window.keypadHeight = 0;
+window.setKeypadSize = (element) => {
+    window.keypadWidth = $(element).outerWidth(true);
+    window.keypadHeight = $(element).outerHeight(true);
+    window.setSidebarSize();
+}
+window.drawerWidth = 0;
+window.drawerHeight = 0;
+window.setDrawerSize = (element) => {
+    if (element) {
+        window.drawerWidth = $(element).outerWidth(true);
+        window.drawerHeight = $(element).outerHeight(true);
+    } else {
+        window.drawerWidth = 0;
+        window.drawerHeight = 0;
+    }
+    window.setSidebarSize();
+}
+window.appbarHeight = 0;
+window.setAppbarHeight = (element) => {
+    window.appbarHeight = $(element).outerHeight(true);
+    window.setSidebarSize();
+}
+window.setSidebarSize = () => {
+    document.documentElement.style.setProperty(`--sidebar-width`, window.keypadWidth + window.drawerWidth);
+    document.documentElement.style.setProperty(`--sidebar-height`, window.keypadHeight + window.drawerHeight + appbarHeight);
 }
 window.doSaveSvgAsPng = (element, name) => {
     saveSvgAsPng(element, name);
+}
+window.scrollToBottom = (element) => {
+    element.scrollTop = element.scrollHeight;
 }
 
 //Prevents backspace except in the case of textareas and text inputs to prevent user navigation.
