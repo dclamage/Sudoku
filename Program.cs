b669457284a2b73b23696833a712d0f22e50a3ed
David Clamage <rangsk@gmail.com>
2021-02-10 01:31:00 -0800

    Updated to MudBlazor 5.0.1

--- Program.cs
+++ Program.cs
@@ -22,9 +22,7 @@ namespace SudokuBlazor
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddWorkerFactory();
-            builder.Services.AddMudBlazorDialog();
-            builder.Services.AddMudBlazorSnackbar();
-            builder.Services.AddMudBlazorResizeListener();
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
